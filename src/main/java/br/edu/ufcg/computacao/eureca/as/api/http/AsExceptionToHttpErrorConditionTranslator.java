59f600a7902ae384bbd34078628046f93f7e1e9e
ielepassos <iele.passos@ccc.ufcg.edu.br>
2022-01-22 12:59:28 -0300

    update classname to match common's changes

--- src/main/java/br/edu/ufcg/computacao/eureca/as/api/http/AsExceptionToHttpErrorConditionTranslator.java
+++ src/main/java/br/edu/ufcg/computacao/eureca/as/api/http/AsExceptionToHttpErrorConditionTranslator.java
@@ -1,8 +1,8 @@
 package br.edu.ufcg.computacao.eureca.as.api.http;
 
-import br.edu.ufcg.computacao.eureca.common.exceptions.ExceptionToHttpErrorConditionTranslator;
+import br.edu.ufcg.computacao.eureca.common.exceptions.CommonExceptionHandler;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
-public class AsExceptionToHttpErrorConditionTranslator extends ExceptionToHttpErrorConditionTranslator {
+public class AsExceptionToHttpErrorConditionTranslator extends CommonExceptionHandler {
 }
